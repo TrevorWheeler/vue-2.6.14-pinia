fad17f7130e34be9f83b27fef8c16ef6f4e154aa
Trevor Wheeler <sendtrevmail@gmail.com>
2022-04-03 20:54:29 +1000

    remove unwanted vue component

--- src/components/home/home.ts
+++ src/components/home/home.ts
@@ -1,6 +1,15 @@
 import { Vue, Component } from "vue-property-decorator";
-
+import { useCounterStore } from "@/store/index";
 @Component({
   components: {},
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  public mounted() {
+    const counter = useCounterStore();
+    console.log("##########");
+    console.log(counter.count);
+    counter.increment();
+    counter.count++;
+    console.log(counter.count);
+  }
+}
